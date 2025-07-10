6fc50a70101b1bc2dad91e78dce9ce8dff5e200b
DraganM03 <draganmijatovic.s1a@gmail.com>
2025-07-10 06:33:00 +0200

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="absolute w-[100%] bg-gray-900 text-white mt-16 py-8 bottom-0">
+  <footer className="w-[100%] bg-gray-900 text-white mt-16 py-8 ">
     <div className="container mx-auto text-center">
       <p>&copy; 2025 Totally Not Udemy. No copyright rights reserved.</p>
     </div>
