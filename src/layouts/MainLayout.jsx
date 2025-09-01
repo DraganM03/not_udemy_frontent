92383cf10e7665e96f5b563a4b3cb80fcd0f4c8e
DraganM03 <draganmijatovic.s1a@gmail.com>
2025-09-01 19:24:30 +0200

    fixed footer to the bottom of the screen

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,9 +5,9 @@ import Footer from '../components/Footer';
 
 const MainLayout = () => {
   return (
-    <div className="bg-gray-50 min-h-screen font-sans">
+    <div className="bg-gray-50 min-h-screen font-sans flex flex-col">
       <Navbar />
-      <main className="p-4 md:p-8">
+      <main className="p-4 md:p-8 flex-1">
         <Outlet />
       </main>
       <Footer />
